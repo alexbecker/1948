24cb1b899fc5d70e2d7311aa763537ea290d4d8e
Alex Becker <acbecker@uchicago.edu>
2017-01-02 09:55:43 -0800

    Add simple test for enc_handler

---
+++ go/src/server/handlers/enc_handler_test.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"server/testingutils"
+	"testing"
+)
+
+var handler http.Handler
+
+func TestMain(m *testing.M) {
+	tempDir, err := ioutil.TempDir("", "enc_handler_test")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(tempDir) // clean up
+	tempFile := filepath.Join(tempDir, "tmp.html")
+	err = ioutil.WriteFile(tempFile, []byte("test"), 0666)
+	if err != nil {
+		panic(err)
+	}
+	handler = EncHandler(tempDir)
+	os.Exit(m.Run())
+}
+
+func TestServeEncFilesNoAccept(t *testing.T) {
+	request := httptest.NewRequest("GET", "/tmp.html", nil)
+	response := httptest.NewRecorder()
+	handler.ServeHTTP(response, request)
+	testingutils.ExpectSuccess(t, 200, "test", response)
+}
